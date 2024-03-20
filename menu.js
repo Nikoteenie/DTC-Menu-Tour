86b84a19c6c996aadc767322815fa795acf68106
Nicole Brodkin <105375963+Nikoteenie@users.noreply.github.com>
2024-03-20 10:41:21 -0400

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,33 +1,78 @@
 
 
-function hideToolTip () {
+var cur = 1;
+
+if (document.getElementById('staticBackdrop')){
+const myModalEl = document.getElementById('staticBackdrop')
+myModalEl.addEventListener('hidden.bs.modal', event => {
+ cur=1;
+})
+}
+
+var modalText= ['Welcome to our new site experience click the next button below ( <span><i class="fwd-txt fa-solid fa-caret-right"></i></span> ) to explore the changes we have made.',
+'Here you can find some our favorite links.',
+'This links will help you navigate your profile, contact us, see your deals and what you have pending in your cart.', 
+'Our menu button opens all the content our site has to offer you.',
+'This menu has been created to make all that we have to offer easily accesible',
+'Menu items designated with a chevron ( <span class="chevron"><i aria-hidden="true" class="fa-solid fa-chevron-right"></i></span> ) will open the subcategories that fall under this category.',
+'Here you can find the subcategories organized and listed for your convenience',
+'If you have any questions about these updates please <a class="contact-link" href="https://www.lifeextension.com/quest-com">Contact Us</a> today.'];
 
 
-    document.getElementById("tooltip").style.display = "none";
-    
-    
-    }
+var modalPageNum = ['New Menu Tour', 'Exposed Links', 'User Info, Contact US & Your Cart', 'Menu Button', 'New Menu', 'Categories', 'Subcategories', 'Questions',]
 
-
-    var cur = 1;
-var arr = ['1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip eprehenderit in.','2. Velit egestas dui id ornare arcu odio ut sem nulla. Tortor condimentum lacinia quis vel eros donec. In fermentum posuere urna nec tincidunt praesent semper feugiat. Sapien et ligula ullamcorper malesuada proin libero. Lectus nulla at volutpat diam ut.','3. Egestas dui id ornare arcu. Massa id neque aliquam vestibulum morbi. Condimentum id venenatis a condimentum vitae. Aliquet risus feugiat in ante metus dictum at tempor commodo.', '4. Magna sit amet purus gravida quis blandit turpis cursus. Vulputate ut pharetra sit amet aliquam id. Vulputate odio ut enim blandit volutpat maecenas. Pulvinar etiam non quam lacus suspendisse faucibus.'];
 function forward() {
+  
   cur=cur+1;
   document.getElementById('bck').disabled = false;
-  if (cur==4) {
-    document.getElementById('fwd').disabled = true
-  }
+
+    if (cur==8) {
+      document.getElementById('fwd').disabled = true
+      document.getElementById('end').disabled = false;
+    }
   document.getElementById('num').textContent = cur;
-  document.getElementById('txt').textContent = arr[cur-1];
-document.getElementById('slide').src="https://auth.lifeextension.com/-/media/lifeextension/scratch/" + cur + ".jpg";
+  document.getElementById('txt').innerHTML= modalText[cur-1];
+  document.getElementById('slide').src="slide-" + cur + ".jpg";
+  document.getElementById('page-modal').textContent = modalPageNum[cur-1];
 }
 
 function back() {
+
   cur=cur-1;
   document.getElementById('fwd').disabled = false;
   if (cur==1) {
     document.getElementById('bck').disabled = true;
+    document.getElementById('end').disabled = true;
   }
  document.getElementById('num').textContent = cur; 
- document.getElementById('txt').textContent = arr[cur-1]; document.getElementById('slide').src="https://auth.lifeextension.com/-/media/lifeextension/scratch/" + cur + ".jpg";
+ document.getElementById('txt').innerHTML = modalText[cur-1]; document.getElementById('slide').src="slide-" + cur + ".jpg";
+ document.getElementById('page-modal').textContent = modalPageNum[cur-1];
 }
+
+function showTourLink () {
+  document.getElementById("tour-link").style.display = "block";
+
+}
+
+function hideToolTip () {
+    document.getElementById("tooltip").style.display = "none"; 
+    }
+
+    function dismissButton () {
+      document.getElementById("tooltip").style.display = "none";
+      document.getElementById("tour-link").style.display = "block";
+    }
+
+  //  localStorage.setItem("time",new Date())
+
+          // function restartModal () {
+         
+          //   document.getElementById("tooltip").style.display = "none";
+          //   document.getElementById("tour-link").style.display = "block"; 
+          //   document.getElementById("tour-link").style.display = "block"; 
+           
+          //   location.reload();
+          // }
+
+          
+
